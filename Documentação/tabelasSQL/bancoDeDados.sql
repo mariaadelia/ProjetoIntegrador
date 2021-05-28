c8c536fc0c5ddfe3a07f685a4751a4eb8deb2058
Yoshi <63078117+YoshiiTsuu@users.noreply.github.com>
2021-05-28 19:55:43 -0300

    Adicionando novos arquivos

--- Documentação/tabelasSQL/bancoDeDados.sql
+++ Documentação/tabelasSQL/bancoDeDados.sql
@@ -7,18 +7,16 @@ id bigint auto_increment,
 nome_completo varchar(100) not null,
 email varchar(100) not null,
 senha varchar(60) not null,
-localizacao varchar(150) not null,
-avaliacao_usuario int,
-vendedor boolean not null,
-consumidor boolean not null,
+validar_senha varchar (60) not null, -- validando a senha
+vendedor boolean not null, -- talvez coloque default
 primary key (id)
 );
 
 create table tb_categoria(
 id bigint auto_increment,
 produtos_servicos varchar(100) not null,
-ativo boolean not null,
-vegano boolean,
+palavras_chave varchar(100),
+descricao varchar(1250),
 primary key (id)
 );
 
@@ -28,12 +26,16 @@ usuario_id bigint,
 categoria_id bigint,
 nome_produto_servico varchar(100) not null,
 preco_produto_servico decimal(8,2) not null,
-quantidade_consumidor int not null,
 estoque_vendedor int,
-localizacao varchar(150) not null,
-descricao_produto_servico varchar(300) not null,
-avaliacao_produto_servico int,
+descricao_produto_servico varchar(1250) not null,
+like_produto_servico bigint,
+dislike_produto_servico bigint,
+midias varchar(1250), -- Imagens + videos do produto
 primary key (id),
 foreign key (usuario_id) references tb_usuario (id),
 foreign key (categoria_id) references tb_categoria (id)
 );
+
+select * from db_ecommerce.tb_usuario;
+select * from db_ecommerce.tb_categoria;
+select * from db_ecommerce.tb_produtos;
